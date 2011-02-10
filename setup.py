7c2b79d17b290b513159c368bc83f2ae6bd12617
alq <alq666@gmail.com>
2011-02-10 00:36:48 -0500

    Submission to pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name = "dogapi",
+    version = "0.0.1",
+    packages = find_packages(),
+    author = "Datadog, Inc.",
+    author_email = "packages@datadoghq.com",
+    description = "Python bindings to Datadog's API",
+    license = "BSD",
+    keywords = "datadog data",
+    url = "http://datadoghq.com",
+)
+
