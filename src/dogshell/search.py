ba3339456fb22a5b6159585f1c3739ad58368277
Matt Singleton <matt@datadoghq.com>
2011-10-05 14:45:49 -0400

    search "query language" for cli

--- src/dogshell/search.py
+++ src/dogshell/search.py
@@ -3,36 +3,22 @@ from dogapi.v1 import SearchService
 from dogshell.common import report_errors, report_warnings, CommandLineClient
 
 class SearchClient(CommandLineClient):
-    
+
     def __init__(self, config):
         self.config = config
 
     def setup_parser(self, subparsers):
         parser_search = subparsers.add_parser('search', help='search datadog')
-        subparsers_search = parser_search.add_subparsers(title='Search domains', help='sub-command help')
+        parser_search.add_argument('query', help='string to search for. accepts faceted or un-faceted queries. see https://github.com/DataDog/dogapi/wiki/Search for the latest on the query language.')
+        parser_search.set_defaults(func=self.search)
 
-        parser_search_hosts = subparsers_search.add_parser('host', help='search datadog for hosts')
-        parser_search_hosts.add_argument('name', help='hostname or hostname fragment to match. implicit wildcards (e.g. *name*).')
-        parser_search_hosts.set_defaults(func=self.search_hosts)
-
-        parser_search_metrics = subparsers_search.add_parser('metric', help='search datadog for metrics')
-        parser_search_metrics.add_argument('name', help='metric name or metric name fragment to match. implicit wildcards (e.g. *name*).')
-        parser_search_metrics.set_defaults(func=self.search_metrics)
-
-    def search_hosts(self, args):
-        svc = SearchService(hosturl)
-        res = svc.query_host(self.apikey, args.name)
+    def search(self, args):
+        svc = SearchService(self.config['apikey'], self.config['appkey'])
+        res = svc.query(args.query)
         report_warnings(res)
         if report_errors(res):
             return
-        for host in res['hosts']:
-            print host['name']
+        for facet, results in res['results'].items():
+            for result in results:
+                print "%s\t%s" % (facet, result)
 
-    def search_metrics(self, args):
-        svc = SearchService(hosturl)
-        res = svc.query_metric(self.apikey, args.name)
-        report_warnings(res)
-        if report_errors(res):
-            return
-        for metric in res['metrics']:
-            print metric['name']
