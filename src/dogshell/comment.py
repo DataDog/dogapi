bb8c779a07d3cb507b8d0ef18aeb560987f3b17d
Matt Singleton <matt@datadoghq.com>
2011-10-05 18:18:50 -0400

    read comments from stdin and make handle optional (implicit)

--- src/dogshell/comment.py
+++ src/dogshell/comment.py
@@ -1,3 +1,5 @@
+import sys
+
 from dogapi.v1 import CommentService
 
 from dogshell.common import report_errors, report_warnings, CommandLineClient
@@ -9,13 +11,15 @@ class CommentClient(CommandLineClient):
 
     def setup_parser(self, subparsers):
         parser = subparsers.add_parser('comment', help='post comments')
-        parser.add_argument('handle', help='handle to post as')
-        parser.add_argument('comment', help='comment to post')
+        parser.add_argument('--handle', help='handle to post as. if unset, posts as the owner of the application key used to authenticate')
+        parser.add_argument('comment', help='comment to post. if unset, reads from stdin.', nargs="?")
         parser.set_defaults(r_func=self.r_comment)
         parser.set_defaults(p_func=self.p_comment)
-        parser.set_defaults(l_func=self.p_comment)
+        parser.set_defaults(l_func=self.l_comment)
 
     def _comment(self, handle, comment):
+        if comment is None:
+            comment = sys.stdin.read()
         svc = CommentService(self.config['apikey'], self.config['appkey'])
         return svc.post(handle, comment)
 
@@ -31,8 +35,22 @@ class CommentClient(CommandLineClient):
         report_warnings(res)
         if report_errors(res):
             return
+        message = res['comment']['message']
+        lines = message.split('\n')
+        message = '\n'.join(['    ' + line for line in lines])
         print 'id\t\t' + str(res['comment']['id'])
         print 'url\t\t' + res['comment']['url']
         print 'resource\t' + res['comment']['resource']
         print 'handle\t\t' + res['comment']['handle']
-        print 'message\t\t' + res['comment']['message']
+        print 'message\n' + message
+
+    def l_comment(self, args):
+        res = self._comment(args.handle, args.comment)
+        report_warnings(res)
+        if report_errors(res):
+            return
+        print 'id\t\t' + str(res['comment']['id'])
+        print 'url\t\t' + res['comment']['url']
+        print 'resource\t' + res['comment']['resource']
+        print 'handle\t\t' + res['comment']['handle']
+        print 'message\t\t' + res['comment']['message'].__repr__()
