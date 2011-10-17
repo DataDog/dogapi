cc31f41b0ee2bf335c186ae732dbe07cc249ea56
Matt Singleton <matthew.j.singleton@gmail.com>
2011-10-16 23:12:26 -0400

    update setup.py to work with easy_install

--- setup.py
+++ setup.py
@@ -1,38 +1,25 @@
 from setuptools import setup, find_packages
 import sys
 
+reqs = []
+reqs.append("sphinx>=1.1.0")
+reqs.append("simplejson>=2.2.1")
+reqs.append("decorator>=3.3.2")
+if sys.version_info < (2, 7):
+    reqs.append("argparse>=1.2")
+
 setup(
     name = "dogapi",
     version = "0.1.0",
-    packages = find_packages("src", exclude=["dogshell"]),
+    packages = find_packages("src"),
     package_dir = {'':'src'},
     author = "Datadog, Inc.",
     author_email = "packages@datadoghq.com",
-    description = "Python bindings to Datadog's API",
+    description = "Python bindings to Datadog's API and a user-facing command line tool.",
     license = "BSD",
     keywords = "datadog data",
     url = "http://datadoghq.com",
-    install_requires= [
-        "sphinx==1.1.0"
-    ],
-)
-
-shell_reqs = ["dogapi==0.1.0"]
-if sys.version_info < (2, 7):
-    shell_reqs.append("argparse>=1.2")
-
-setup(
-    name = "dogshell",
-    version = "0.1.0",
-    packages = ["dogshell"],
-    package_dir = {'':'src'},
-    author = "Datadog, Inc.",
-    author_email = "packages@datadoghq.com",
-    description = "A user-facing command line tool, 'dog', for interacting with the Datadog API.",
-    license = "BSD",
-    keywords = "datadog data",
-    url = "http://datadoghq.com",
-    install_requires=shell_reqs,
+    install_requires= reqs,
     entry_points={
         'console_scripts': [
             'dog = dogshell:main',
