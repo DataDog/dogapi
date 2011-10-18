9aab888dfadd100dfc9b260c975d365edf3174ee
Matt Singleton <matthew.j.singleton@gmail.com>
2011-10-18 18:34:37 -0400

    bumping version to 0.9.x (aka pre-1.0.x)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name = "dogapi",
-    version = "0.1.0",
+    version = "0.9.0",
     packages = find_packages("src"),
     package_dir = {'':'src'},
     author = "Datadog, Inc.",
