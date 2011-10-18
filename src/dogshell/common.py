1491d311b13037a2bded82a348adfdfd52e1ada3
Carlo Cabanilla <carlo@datadoghq.com>
2011-10-18 16:11:02 -0400

    walk the user through creating a dogrc file if none is found

--- src/dogshell/common.py
+++ src/dogshell/common.py
@@ -1,4 +1,5 @@
 import ConfigParser
+import os
 import sys
 from UserDict import IterableUserDict
 
@@ -23,6 +24,39 @@ class DogshellConfig(IterableUserDict):
 
     def load(self, config_file):
         config = ConfigParser.ConfigParser()
-        config.read(config_file)
+        
+        if os.access(config_file, os.F_OK):
+            config.read(config_file)
+            if not config.has_section('Connection'):
+                report_errors({'errors': ['%s has no [Connection] section' % config_file]})
+        else:
+            try:
+                response = raw_input('%s does not exist. Would you like to create it? [Y/n] ' % config_file)
+                if response.strip().lower() in ['', 'y', 'yes']:
+                    # Read the api and app keys from stdin
+                    apikey = raw_input('What is your api key? (Get it here: https://app.datadoghq.com/account/settings) ')
+                    appkey = raw_input('What is your application key? (Generate one here: https://app.datadoghq.com/account/settings) ')
+                
+                    # Write the config file
+                    config.add_section('Connection')
+                    config.set('Connection', 'apikey', apikey)
+                    config.set('Connection', 'appkey', appkey)
+                
+                    f = open(config_file, 'w')
+                    config.write(f)
+                    f.close()
+                    print 'Wrote %s' % config_file
+                        
+                else:
+                    # Abort
+                    print >> sys.stderr, 'Exiting'
+                    sys.exit(1)
+            except KeyboardInterrupt:
+                # Abort
+                print >> sys.stderr, '\nExiting'
+                sys.exit(1)
+            
+        
         self['apikey'] = config.get('Connection', 'apikey')
         self['appkey'] = config.get('Connection', 'appkey')
+            
