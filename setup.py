d964d51c233efcec3aed9bda7ff83a8d2f51746f
David Ormsbee <dave@datadoghq.com>
2011-10-26 18:06:37 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name = "dogapi",
-    version = "0.9.4",
+    version = "0.9.6",
     packages = find_packages("src"),
     package_dir = {'':'src'},
     author = "Datadog, Inc.",
