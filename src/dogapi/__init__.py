626eec0144f46e385813f9bd5921eb15e7ddcb48
Carlo Cabanilla <carlo@datadoghq.com>
2012-02-17 12:08:40 -0500

    remove old pre v1 code

--- src/dogapi/__init__.py
+++ src/dogapi/__init__.py
@@ -1,11 +1,6 @@
 import logging
 logging.basicConfig()
 
-import common
-import facade
-import metric
-import event
-from facade import init
 from simpleclient import SimpleClient
 
 dog = SimpleClient()
