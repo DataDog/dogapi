f28e8e5afe7f20a835a88050ab33b04f53146f41
Matt Perpick <matthewperpick@gmail.com>
2012-04-06 14:40:04 -0400

    Measuring the impact of dogapi.

--- tests/performance/test_stats_api_performance.py
+++ tests/performance/test_stats_api_performance.py
@@ -2,19 +2,78 @@
 Performance tests for the stats api.
 """
 
-import hotshot
-import hotshot.stats
 import os
+import random
 import tempfile
 import time
+
+import yappi
 
 from dogapi import DogStatsApi
 
 
 class NullReporter(object):
+    """
+    A DogAPI to nowhere.
+    """
 
     def flush(self, metrics):
+        print 'flushing metrics'
+
+
+class NullDogStatsApi(DogStatsApi):
+    """
+    A DogStats API that does nothing, for comparing the effects
+    of including DogApi in your program.
+    """
+
+    def start(self, *args, **kwargs):
         pass
+
+    def stop(self, *args, **kwargs):
+        pass
+
+    def gauge(self, *args, **kwargs):
+        pass
+
+    def increment(self, *args, **kwargs):
+        pass
+
+    def histogram(self, *args, **kwargs):
+        pass
+
+
+class CPUBoundProgram(object):
+
+    def __init__(self, dog_stats_api):
+        self.dog_stats_api = dog_stats_api
+
+    def run(self):
+        for i in range(100000):
+            self.dog_stats_api.gauge('current.number', i)
+            self.dog_stats_api.increment('numbers.checked')
+            j = 0
+            start = time.time()
+            while j < i:
+                j += 1
+            self.dog_stats_api.histogram('number.check.time', time.time() - start)
+
+
+def profile_cpu_bound_program():
+    real_dog = DogStatsApi()
+    real_dog.reporter = NullReporter()
+    fake_dog = NullDogStatsApi()
+    for type_, dog in [('real', real_dog), ('fake', fake_dog)]:
+        print '\n\n\nTESTING %s\n\n' % type_
+        dog.start()
+        program = CPUBoundProgram(dog)
+        yappi.start()
+        program.run()
+        yappi.print_stats(sort_type=yappi.SORTTYPE_TSUB, sort_order=yappi.SORTORDER_DESC)
+        yappi.stop()
+        yappi.clear_stats()
+
+
 
 def measure_thousands_of_metrics():
     dog = DogStatsApi()
@@ -27,25 +86,8 @@ def measure_thousands_of_metrics():
             dog.histogram('histogram.%s' % name, j)
         print 'run %s' % i
 
-def profile_hotshot():
-    _, logfile = tempfile.mkstemp()
-    try:
-        profiler = hotshot.Profile(logfile)
-        profiler.runcall(measure_thousands_of_metrics)
-        profiler.close()
-        stats = hotshot.stats.load(logfile)
-        stats.strip_dirs()
-        stats.sort_stats('cumulative', 'time', 'calls')
-        stats.print_stats()
-    finally:
-        if os.path.exists(logfile):
-            os.remove(logfile)
-
 def profile_yappi():
-    import yappi
-    yappi.start()
-    measure_thousands_of_metrics()
-    yappi.print_stats(sort_type=yappi.SORTTYPE_TSUB, sort_order=yappi.SORTORDER_DESC)
+    profile_cpu_bound_program()
 
 
 
