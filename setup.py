f6e9d99822d8cb05cee1f3dfbf39222008c96eb7
Matt Perpick <matthewperpick@gmail.com>
2012-04-06 14:40:36 -0400

    Bumping version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name = "dogapi",
-    version = "1.0.1",
+    version = "1.0.2",
     packages = find_packages("src"),
     package_dir = {'':'src'},
     author = "Datadog, Inc.",
