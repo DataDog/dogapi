fb6a096df03a2b6c552c206be5c1d70bfdf0f0b8
alq <alq@datadoghq.com>
2012-05-29 19:00:35 +0000

    Fixing setup.py for python 2.6

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 import sys
+import types
 
 reqs = []
-if sys.version_info.major == 2:
+if type(sys.version_info) == types.TupleType and sys.version_info[0] ==2 or sys.version_info.major == 2:
     # simplejson is not python3 compatible
     reqs.append("simplejson>=2.0.9")
 reqs.append("decorator>=3.3.2")
