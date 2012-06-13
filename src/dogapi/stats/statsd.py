f819df5a7e946148eb65ce8288c7632ba3446a6f
Matt Perpick <matthewperpick@gmail.com>
2012-06-13 13:08:45 -0400

    Inline tight function call.

--- src/dogapi/stats/statsd.py
+++ src/dogapi/stats/statsd.py
@@ -12,6 +12,7 @@ class StatsdAggregator(object):
         self.port = int(port)
         self.address = (self.host, self.port)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket_sendto = self.socket.sendto
 
     def add_point(self, metric, tags, timestamp, value, metric_class, sample_rate=1):
         payload = '%s:%s|%s' % (metric, value, metric_class.stats_tag)
@@ -21,4 +22,4 @@ class StatsdAggregator(object):
             payload += '|@%s' % sample_rate
         if tags:
             payload += '|#' + ','.join(tags)
-        self.socket.sendto(payload, self.address)
+        self.socket_sendto(payload, self.address)
