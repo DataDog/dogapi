80466d1940b7b86db997aef45cd45d99b3574ae7
Matt Perpick <matthewperpick@gmail.com>
2012-09-21 14:36:36 -0400

    Adding test keys to rakefile.

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,6 +1,11 @@
 
 ENV['PYTHONPATH'] = './src:' + (ENV['PYTHONPATH'] or '')
 
+# Assign some test keys if they aren't already set.
+ENV["DATADOG_API_KEY"] ||= '9775a026f1ca7d1c6c5af9d94d9595a4'
+ENV["DATADOG_APP_KEY"] ||= '87ce4a24b5553d2e482ea8a8500e71b8ad4554ff'
+
+
 task :default => [:clean, :test, :build]
 task :clean => [:clean_pyc, :clean_build, :clean_dist]
 
