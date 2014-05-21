46dda5e761d3752fce26b379cc8542e3f5244376
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2014-05-21 11:10:17 -0400

    Make sure @ notify is the first decorator
    fixes #91

--- examples/fabfile.py
+++ examples/fabfile.py
@@ -8,14 +8,14 @@ from dogapi.fab import setup, notify
 setup(api_key = "YOUR API KEY HERE")
 
 # Make sure @notify is just below @task
-@task(default=True, alias="success")
 @notify
+@task(default=True, alias="success")
 def sweet_task(some_arg, other_arg):
     """Always succeeds"""
     print(green("My sweet task always runs properly."))
 
-@task(alias="failure")
 @notify
+@task(alias="failure")
 def boring_task(some_arg):
     """Always fails"""
     print(red("My boring task is designed to fail."))
@@ -25,8 +25,8 @@ env.roledefs.update({
     'webserver': ['localhost']
 })
 
-@task(alias="has_roles")
 @notify
+@task(alias="has_roles")
 @roles('webserver')
 @hosts('localhost')
 def roles_task(arg_1, arg_2):
