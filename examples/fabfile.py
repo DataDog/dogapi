b81ee592f9ce9e68dcc5e94df624251fdfdc3e87
Carlo Cabanilla <carlo@datadoghq.com>
2014-05-22 16:20:35 +0000

    @ notify doesn't actually have to be first
    see #59
    
    Revert "Make sure @ notify is the first decorator"
    
    This reverts commit 46dda5e761d3752fce26b379cc8542e3f5244376.

--- examples/fabfile.py
+++ examples/fabfile.py
@@ -8,14 +8,14 @@ from dogapi.fab import setup, notify
 setup(api_key = "YOUR API KEY HERE")
 
 # Make sure @notify is just below @task
-@notify
 @task(default=True, alias="success")
+@notify
 def sweet_task(some_arg, other_arg):
     """Always succeeds"""
     print(green("My sweet task always runs properly."))
 
-@notify
 @task(alias="failure")
+@notify
 def boring_task(some_arg):
     """Always fails"""
     print(red("My boring task is designed to fail."))
@@ -25,8 +25,8 @@ env.roledefs.update({
     'webserver': ['localhost']
 })
 
-@notify
 @task(alias="has_roles")
+@notify
 @roles('webserver')
 @hosts('localhost')
 def roles_task(arg_1, arg_2):
